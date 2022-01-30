4f44064f38d5749babf191531a70b1f472a3c2c7
Geroge Rupp <a0919329@gmail.com>
2022-01-31 01:26:40 +0800

    Change function name
    Remove status checker and useless message

--- fetch.py
+++ fetch.py
@@ -1,66 +1,40 @@
 from datetime import datetime
 
-async def getID(session, rcode:bool=False):
+async def GetNewestEvent(session):
     print(f"fetching event id at: {str(datetime.now()):0.22}")
     async with session.get("https://api.matsurihi.me/mltd/v1/events") as response:
-        if rcode:
-            print(f"response code: {response.status}")
-        if response.status == 200:
-            try:
-                data =  await response.json()
-                return data[-1]["id"]
-            except Exception as e:
-                print(f"exception occur: {e}")
-        else:
-            print(f"bad response status: \n{response.status}")
-    
-async def getEventInfo(evtid, session, rcode:bool=False):
+        try:
+            data = await response.json()
+            return data[-1]
+        except Exception as e:
+            print(f"exception occur: {e}")
+
+async def SearchEvent(evtid, session):
     async with session.get(f"https://api.matsurihi.me/mltd/v1/events/{evtid}") as response:
         print(f"fetching event information at: {str(datetime.now()):0.22}")
-        if rcode:
-            print(f"response code: {response.status}")
-        if response.status == 200:
-            try:
-                data = await response.json()
-                return data
-            except Exception as e:
-                print(f"exception occur: {e}")
-        else:
-            print(f"bad response status: \n{response.status}")
-        if rcode:
-            print(f"response code: {response.status}")
+        try:
+            data = await response.json()
+            return data
+        except Exception as e:
+            print(f"exception occur: {e}")
     
-async def getRankBorder(evtid, session, rcode:bool=False):
+async def FetchBorder(evtid, session):
     async with session.get(f"https://api.matsurihi.me/mltd/v1/events/{evtid}/rankings/borderPoints") as response:
         print(f"fetching event border at: {str(datetime.now()):0.22}")
-        if rcode:
-            print(f"response code: {response.status}")
-        if response.status == 200:
-            try:
-                data = await response.json()
-                return data
-            except Exception as e:
-                print(f"exception occur: {e}")
-        else:
-            print(f"bad response status: \n{response.status}")
-        if rcode:
-            print(f"response code: {response.status}")
-    
-async def fetchCover(session, evtid, rcode:bool=False):
-    print(f"fetching event cover image at: {str(datetime.now()):0.22}")
+        try:
+            data = await response.json()
+            return data
+        except Exception as e:
+            print(f"exception occur: {e}")
+
+async def FetchCover(session, evtid):
     async with session.get(f"https://storage.matsurihi.me/mltd/event_bg/{evtid:0>4,d}.png") as response:
-        if rcode:
-            print(f"response code: {response.status}")
-        if response.status == 200:
-            try:
-                with open(f"{evtid:0>4,d}.png", "wb") as file:
-                    file.write(await response.read())
-            except Exception as e:
-                print(f"exception occur: {e}")
-        else:
-            print(f"bad response status: \n{response.status}")
-        if rcode:
-            print(f"response code: {response.status}")
+        print(f"fetching event cover image at: {str(datetime.now()):0.22}")
+        try:
+            with open(f"{evtid:0>4,d}.png", "wb") as file:
+                file.write(await response.read())
+        except Exception as e:
+            print(f"exception occur: {e}")
 
 if __name__ == "__main__":
     print('using "main.py" instead.')
