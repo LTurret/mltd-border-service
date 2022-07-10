c1983a4de8e36c949de85e962fb82c86104c011f
LTurret <a0919329@gmail.com>
2022-07-10 10:58:41 +0800

    procedure change and rename variable

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-O", "--output_path", nargs=1, type=str, metavar="",
                     required=False,
                     default="image",
-                    help='Image generate ouput path, default is "./image"'
+                    help='Image generate output path, default is "./image"'
                 )
 parser.add_argument("-I", "--identify", nargs=1, type=int, metavar="",
                     required=False,
@@ -39,60 +39,64 @@ group.add_argument("--static",
 )
 opt = parser.parse_args()
 
-async def main(output_type:list, output_path:str, checksum:bool=False, dryrun:bool=False, static:bool=False, identify:int=None):
+async def main(output_type: list = ["pt"], output_path: str = "./image", checksum: bool = False, dryrun: bool = False, static: bool = False, identify = None):
 
-    check_identified = lambda identify: True if (identify is not None and type(identify) is int and identify > 0) else 1
+    check_identified = lambda identify: True if (identify is not None and type(identify) is int and identify > 0) else False
     identify_maximun = lambda identify, idmax: True if (identify is not None and identify > idmax) else 2
     matchtype = lambda typecode: ([3, 4, 5, 11, 13, 16].count(typecode)) == 1
+    border_exists = lambda file: True if (len(file) > 0) else False
 
+    identify = identify[0]
     tasks = []
     announcement = ""
 
     if not static:
         async with aiohttp.ClientSession() as session:
-            eventdata = await GetNewest(session)
-            identify = eventdata["id"]
+            event_data = await GetNewest(session)
 
             if check_identified(identify):
-                if matchtype(eventdata["type"]):
-                    tasks.append(asyncio.create_task(FetchBorder(identify, session)))
-                else:
-                    announcement = "This event is inborderable."
+                event_data = await Search(identify, session)
+                print("fuck")
             else:
-                identify = eventdata["id"]
-                if matchtype(eventdata["type"]):
-                    tasks.append(asyncio.create_task(FetchBorder(identify, session)))
-            await asyncio.gather(*tasks)
-            if matchtype(eventdata["type"]):
-                border = tasks[0].result()
-            print("fetch data complete.")
+                identify = event_data["id"]
+
+            if matchtype(event_data["type"]):
+                border_data = await FetchBorder(identify, session)
+            else:
+                announcement = "This is an inborderable event."
 
     if checksum:
-        print("checksum complete.")
+        announcement = "checksum complete."
     else:
+
         if not os.path.isdir("./dataset"):
             os.mkdir("./dataset")
+
         os.chdir("./dataset")
         tasks = [
-            asyncio.create_task(makefile(eventdata, "information"))
+            asyncio.create_task(makefile(event_data, "information"))
         ]
-        if matchtype(eventdata["type"]):
-            tasks.append(asyncio.create_task(makefile(border, "border")))
+
+        if border_exists(border_data):
+            tasks.append(asyncio.create_task(makefile(border_data, "border")))
+
         await asyncio.gather(*tasks)
         os.chdir("../")
 
         if dryrun:
-            print("dryrun complete.")
+            announcement = "dryrun complete."
         else:
+
             if not os.path.isdir(output_path):
                 os.mkdir(output_path)
-
             tasks = []
             for category in output_type:
                 tasks.append(makeimg(category, output_path))
             await asyncio.gather(*tasks)
 
+    print(announcement)
+
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    loop.run_until_complete(main(opt.output_type, opt.output_path, opt.checksum, opt.dryrun, opt.static, opt.search_id))
+    loop.run_until_complete(main(opt.type, opt.output_path, opt.checksum, opt.dryrun, opt.static, opt.identify))
--- src/make.py
+++ src/make.py
@@ -1,10 +1,10 @@
 import json
 
-async def makefile(file:json, filename:str):
+async def makefile(file:list, filename:str):
     try:
         with open(f"{filename}.json", mode="w", encoding="utf-8") as data:
             data = json.dump(file, data, indent=4, ensure_ascii=False)
-    except Exception as e:
-        print(f"issue occur: {e}")
+    except Exception as exception:
+        print(f"issue occur: {exception}")
     else:
-        print(f'file "{filename}.json" created.')
+        print(f'"{filename}.json" created.')
